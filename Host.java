ae392ebda8c5ec7ff66c09d7ae62fe657f5db234
akarimcheese <akarim1@terpmail.umd.edu>
2016-05-22 23:20:29 -0400

    Update Host.java

--- Host.java
+++ Host.java
@@ -6,13 +6,13 @@
    public class Host extends JPanel
    {
       public static JLabel faceLabel = new JLabel("");
-      public static ImageIcon face1 = new ImageIcon("Howie1.png");
-      public static ImageIcon face2 = new ImageIcon("Howie2.png");
-      public static ImageIcon face3 = new ImageIcon("Howie3.png");
-      public static ImageIcon face4 = new ImageIcon("Howie4.png");
-      public static ImageIcon face5 = new ImageIcon("Howie5.png");
-      public static ImageIcon caller1 = new ImageIcon("banker1.png");
-      public static ImageIcon caller2 = new ImageIcon("banker2.png");
+      public static ImageIcon face1 = new ImageIcon("img/Howie1.png");
+      public static ImageIcon face2 = new ImageIcon("img/Howie2.png");
+      public static ImageIcon face3 = new ImageIcon("img/Howie3.png");
+      public static ImageIcon face4 = new ImageIcon("img/Howie4.png");
+      public static ImageIcon face5 = new ImageIcon("img/Howie5.png");
+      public static ImageIcon caller1 = new ImageIcon("img/banker1.png");
+      public static ImageIcon caller2 = new ImageIcon("img/banker2.png");
    // I provide the images and label to be static so that it can be changed through
    // the briefcase action listener
    
@@ -21,7 +21,7 @@
       {
          setLayout(new FlowLayout());
          
-         ImageIcon face1 = new ImageIcon("Howie1.png");
+         ImageIcon face1 = new ImageIcon("img/Howie1.png");
          faceLabel.setIcon(face1);
          add(faceLabel);
       
